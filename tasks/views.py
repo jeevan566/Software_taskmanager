f25dd5c6d808fc8c1038715402bcd505802b4cfd
jeevan566 <jeevan56jeevan56@gmail.com>
2025-03-06 18:23:51 +0530

    Add files via upload

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,4 @@
+##JEEVAN PROJECT
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.forms import UserCreationForm
